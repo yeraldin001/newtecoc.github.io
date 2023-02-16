e97c02c800fdbf7719aa0ea1fca8bc429dc0aabd
ANGIE YERALDINE HIGUITA DAVID <est.angiehiguita@tecoc.edu.co>
2023-02-16 16:26:24 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('#hamburger');
+const close = document.querySelector('#close');
+const menu = document.querySelector('#menu');
+
+hamburger.addEventListener('click',()=>{
+    hamburger.style.display = 'none';
+    menu.style.display = 'block';
+    close.style.display = 'block';
+
+});
+
+hamburger.addEventListener('click',()=>{
+    hamburger.style.display = 'block';
+    menu.style.display = 'none';
+    close.style.display = 'none';
+
+});
